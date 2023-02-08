5bb582fbb3d2d18f1256d7e74e6d6211fedeed56
Manan Doshi <manandoshi1301@gmail.com>
2023-02-08 10:41:25 +0530

    First commit with navbar and banner

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import styles from "./Banner.module.css";
+import { Col, Container, Image, Row, Stack } from 'react-bootstrap';
+import highResMGImg from "../../images/mg-hr-green-nobg.png";
+import { ImArrowDown2 } from 'react-icons/im';
+
+const Banner = () => {
+
+  const BannerDataCol = () => {
+    return (
+      <Col className={styles.bannerDataCol}>
+        <Container>
+          <Stack gap={4}>
+            <div>
+              <Image src={highResMGImg} fluid />
+            </div>
+            <div>
+              <Stack gap={2}>
+                <h1 className='fw-bolder'>
+                  Bringing innovation to a AI industry...
+                </h1>
+                <div>
+                  <ImArrowDown2 />
+                </div>
+              </Stack>
+            </div>
+          </Stack>
+        </Container>
+      </Col>
+    )
+  }
+
+  const BannerImgCol = () => {
+    return (
+      <Col className={styles.bannerImgCol}></Col>
+    )
+  }
+
+  return (
+    <>
+      <Container fluid>
+        <Row>
+          <BannerDataCol />
+          <BannerImgCol />
+        </Row>
+      </Container>
+    </>
+  )
+}
+
+export default Banner
---
+++ src/components/common/WebNavbar.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+// import logoImg from "../../images/mg-hr-green-nobg.png";
+import logoImg from "../../images/mg.svg";
+// import Button from 'react-bootstrap/Button';
+// import Form from 'react-bootstrap/Form';
+// import NavDropdown from 'react-bootstrap/NavDropdown';
+
+const WebNavbar = () => {
+
+
+  const navLinks = [
+    { title: "About" },
+    { title: "Projects" },
+    { title: "Work" },
+    { title: "Skills" },
+    { title: "Contact" },
+  ];
+
+  return (
+    <>
+      {['md'].map((expand) => (
+        <Navbar key={expand} expand={expand} className="mb-0 p-0">
+          <Container fluid className='p-2'>
+            {/* <Navbar.Brand href="#">Navbar Offcanvas</Navbar.Brand> */}
+            <Navbar.Brand href="#home">
+              <img
+                alt=""
+                src={logoImg}
+                width="45"
+                height="45"
+                className="d-inline-block align-top"
+              />{' '}
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+            <Navbar.Offcanvas
+              id={`offcanvasNavbar-expand-${expand}`}
+              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+              placement="end"
+            >
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                  Offcanvas
+                </Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                <Nav className="justify-content-end flex-grow-1 pe-3">
+                  {
+                    navLinks.map((obj, key) => (
+                      <Nav.Link href={`#action${key}`}>{obj.title.toUpperCase()}</Nav.Link>
+                    ))
+                  }
+                  {/* <NavDropdown
+                    title="Dropdown"
+                    id={`offcanvasNavbarDropdown-expand-${expand}`}
+                  >
+                    <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                    <NavDropdown.Item href="#action4">
+                      Another action
+                    </NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action5">
+                      Something else here
+                    </NavDropdown.Item>
+                  </NavDropdown> */}
+                </Nav>
+                {/* <Form className="d-flex">
+                  <Form.Control
+                    type="search"
+                    placeholder="Search"
+                    className="me-2"
+                    aria-label="Search"
+                  />
+                  <Button variant="outline-success">Search</Button>
+                </Form> */}
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+          </Container>
+        </Navbar>
+      ))}
+    </>
+  );
+}
+
+export default WebNavbar
