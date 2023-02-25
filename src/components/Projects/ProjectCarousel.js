94b847db9c0b0f60c9c0c1d9a9e8cc04fdb402b8
Manan Doshi <manandoshi1301@gmail.com>
2023-02-25 22:42:23 +0530

    Added projectcarousel

--- src/components/Projects/ProjectCarousel.js
+++ src/components/Projects/ProjectCarousel.js
@@ -1,32 +1,31 @@
-import React from 'react';
-import Carousel from 'react-bootstrap/Carousel';
-import projectorImg from "../../images/projectorImg.png";
+import React from 'react'
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
 
 const ProjectCarousel = () => {
 
-  const CarouselComp = () => {
+  const data = [
+    { title: "0", },
+    { title: "1", },
+    { title: "2", },
+  ];
+  const CarouselTile = ({ item }) => {
     return (
-      <Carousel>
-        <Carousel.Item>
-          <img
-            className="d-block w-100 img-fluid"
-            src={projectorImg}
-            alt="First slide"
-          />
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100 img-fluid"
-            src={projectorImg}
-            alt="Second slide"
-          />
-        </Carousel.Item>
-      </Carousel>
+      <div>
+        <img src={"https://images.ctfassets.net/hrltx12pl8hq/5mO7yOCen5hp7ZzjxKEiUK/3edb66cca1df3c6c7b458d98dfe6a3f2/aerial-shutterstock_1303849930.jpg?fit=fill&w=1024&h=683&fm=webp"} />
+        <p className="legend">Legend 1</p>
+      </div>
     )
   }
   return (
     <>
-      <CarouselComp />
+      <Carousel>
+        {
+          data.map((item, key) => (
+            <CarouselTile item={item} />
+          ))
+        }
+      </Carousel>
     </>
   )
 }
