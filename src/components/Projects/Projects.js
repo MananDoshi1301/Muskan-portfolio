05a68305586365017f72dcac33cdfe0bedc8cbdb
Manan Doshi <manandoshi1301@gmail.com>
2023-02-25 22:42:36 +0530

    Impported project carousel

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import cx from "classnames";
-import styles from "./Projects.module.scss";
+import styles from "./Projects.module.css";
 import ProjectCarousel from './ProjectCarousel';
 
 const Projects = () => {
   return (
     <>
-      <Container className={cx("position-relative", styles.projectsContainer)} fluid>
-        <Row>
-          <Col>
-            <h1 className='display-2 fw-bold'>My Projects</h1>
+      <Container className={cx("", styles.projectsContainer)} fluid>
+        <Row className={cx(styles.projectHeader)}>
+          <Col xl={6} lg={6} md={6} sm={12}>
+            <h1 className='sectionHeader display-2 fw-bold'>My Projects</h1>
           </Col>
+          <div className={cx(styles.overlayCarouselDiv)}>
+            <ProjectCarousel />
+          </div>
+          <Col xl={6} lg={6} md={6} sm={12} className={cx(styles.projectBgCol, '')}>
 
-          <Col className={cx(styles.projectBgCol, '')}>
           </Col>
         </Row>
-        <Container className={cx(styles.projectCarouselContainer, 'position-absolute')}>
-          <ProjectCarousel />
-        </Container>
       </Container>
     </>
   )
