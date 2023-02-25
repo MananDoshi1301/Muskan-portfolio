752801a733810f75700ab4db90c7494ed4152338
Manan Doshi <manandoshi1301@gmail.com>
2023-02-25 22:02:50 +0530

    Added app common css file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // import logo from './logo.svg';
 import './App.scss';
+import './App.css';
 import cx from "classnames";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import WebNavbar from './components/common/Navbar/WebNavbar';
